52e24a44cc6c099bae94d4290330c63a81130b13
mkirbst <mkirbstqgmail.com>
2014-09-04 14:38:23 +0200

    first code version added

---
+++ top10.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>moerbst.de TS3-Top10 </title>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+
+<div id="container">
+
+        <table class="zebra">
+    <caption><a href=http://moerbst.de>moerbst.de Teamspeak3 Camper Top10</a></caption>
+                <thead>
+                <tr>
+                                <th>Platz</th>
+                                <th>Name</th>
+                                <th>Teamspeak-ID</th>
+                                <th>Zeit</th>
+            </tr>
+                </thead>
+        <tbody>
+<?php
+$COUNTER=1;
+
+function secondsToTime($inputSeconds) {
+
+    $secondsInAMinute = 60;
+    $secondsInAnHour  = 60 * $secondsInAMinute;
+    $secondsInADay    = 24 * $secondsInAnHour;
+
+    // extract days
+    $days = floor($inputSeconds / $secondsInADay);
+
+    // extract hours
+    $hourSeconds = $inputSeconds % $secondsInADay;
+    $hours = floor($hourSeconds / $secondsInAnHour);
+
+    // extract minutes
+    $minuteSeconds = $hourSeconds % $secondsInAnHour;
+    $minutes = floor($minuteSeconds / $secondsInAMinute);
+
+    // extract the remaining seconds
+    $remainingSeconds = $minuteSeconds % $secondsInAMinute;
+    $seconds = ceil($remainingSeconds);
+
+    // return the final array
+    $obj = array(
+        'd' => (int) $days,
+        'h' => (int) $hours,
+        'm' => (int) $minutes,
+        's' => (int) $seconds,
+    );
+        $res= $days."d".$hours."h".$minutes."m";
+        return $res;
+//        return $days." Tage ".$hours." Stunden ";
+}
+
+
+
+// create database connection, credentials same as in perl script
+$link = mysql_connect("127.0.0.1", "myts3MysqlUser", "MySqlPassword")
+    or die("mysql connection error: " . mysql_error());
+# echo "mysql connection successful";
+mysql_select_db("munin") or die("could not select specified database");
+
+// execute the mysql statement
+$query = "SELECT CLNAME,CLDBID,Minutes FROM ts3top  WHERE clname NOT LIKE '%bot%' AND clname NOT LIKE  '%127.0.0.1%' ORDER BY Minutes DESC, CLDBID ASC";
+$result = mysql_query($query) or die("Anfrage fehlgeschlagen: " . mysql_error());
+
+// print the output as html
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    $n=0;
+    echo "\t<tr>\n";
+
+    echo "<td>".$COUNTER++."</td>";
+    foreach ($line as $col_value) {
+                                $n++;
+
+        echo "\t\t<td>";
+
+                                //count to 3. tab format only this value
+        if($n == 3) {
+                echo secondsToTime($col_value*60)." (".$col_value."min)";
+        } else {
+                echo $col_value;
+                                }
+
+        echo "</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+
+// now free the result set
+mysql_free_result($result);
+
+// close connection
+mysql_close($link);
+
+?>
+
+        </tbody>
+        </table>
+</div>
+
+</body>
+</html>
+
+
---
+++ ts3usersToMysql.pl
@@ -0,0 +1,101 @@
+#!/usr/local/bin/perl
+
+# Teamspeak 3 Users Top10 per time on server
+# this script has to be executed every 5 minutes and polls the users currently on the server
+# if you want another timespan as 5 minutes, please modify the mysql statement at the end
+#
+# create a query login name+password in your TS3 server and also 
+# a appropriate database as following:
+# CREATE TABLE IF NOT EXISTS `munin`.`ts3top` (`CLDBID` INT NOT NULL ,  `CLNAME` VARCHAR(64) NOT NULL ,  `Minutes` BIGINT,  PRIMARY KEY (`CLDBID`)) ENGINE = InnoDB DEFAULT CHARACTER SET = u$
+# dont forget to grant your mysql user privileges for this database
+
+use strict;
+use Net::Telnet;
+use DBI;
+
+my ($db_user, $db_name, $db_pass) = ('myts3mysqluser', 'myts3mysqldb', 'mySecretMysqlPassword');
+
+
+#################
+## telnet part ##
+#################
+
+# establish some global variables
+my $HOSTNAME = "127.0.0.1";
+my $HOSTPORT = "10011";
+my $logfile  = "ts3perl.log";
+
+my $telnet = new Net::Telnet ( Timeout=>10, Errmode=>'die', Input_log => $logfile);
+$telnet->open(Host => $HOSTNAME, Port => $HOSTPORT);
+$telnet->waitfor('/Welcome */i');
+$telnet->print('login ts3queryUsername ts3queryPassword');
+$telnet->waitfor('/error id=0 msg=ok/i');
+$telnet->print('use sid=1');
+$telnet->waitfor('/error id=0 msg=ok/i');
+$telnet->print("clientlist");
+my @TELNETRAW = $telnet->waitfor('/error id=0 msg=ok/i');
+$telnet->close;
+
+##############################
+## string modification part ##
+##############################
+
+my @clients = split( '\|' , @TELNETRAW[0]);
+
+my $CLDBID      = "";
+my $CLNAME      = "";
+
+#### open connection to database
+my $dbh = DBI->connect("DBI:mysql:database=$db_name", $db_user, $db_pass);
+
+foreach my $client ( @clients )
+{
+        # process client only if clienttype 0 - normal teamspeak client
+        if ($client =~ m/client_type=0/)
+        {
+#               print "$client \n\n";
+
+                my @clientline = split(' ', $client);
+
+
+                ##              
+                foreach my $clientpart ( @clientline )
+                {
+                        ## process client_datbase_id
+                        if ($clientpart =~ m/^client_database_id=/)
+                        {
+                                # remove "client_database_id=" from client_database_id=42 
+                                my @TMPCLDBID = split("=", $clientpart);
+                                $CLDBID = @TMPCLDBID[1]
+                        }
+
+                        ## process client_nickname
+                        if ($clientpart =~ m/^client_nickname=/)
+                        {
+                                # remove trailing client_nickname= from string
+                                my @TMPCLNAME = split("=", $clientpart);
+                                $CLNAME = @TMPCLNAME[1];
+                                ## clean up TS names from st**id id**ts who use every special char UTF16 has available in their TS names ....
+                                $CLNAME =~ s/[^a-zA-Z0-9._-]/_/g;
+                        }
+
+                }
+
+                ## INSERT INTO DATABASE
+
+                my $sth = $dbh->prepare('INSERT INTO ts3top (CLDBID, CLNAME, Minutes) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE CLNAME=? ,Minutes=Minutes+5 ');
+
+                $sth->execute($CLDBID, $CLNAME, 5, $CLNAME) or die $DBI::errstr;;
+
+                $sth->finish();
+
+        }
+}
+        
+## disconnect from database
+$dbh->disconnect();
+
+                
+
+
+               
